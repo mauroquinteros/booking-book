d6c60eec8e0a3f0fae88bfc36d35bf03f15710b3
mauroquinteros <quinterosmauro0599@gmail.com>
2020-08-24 22:02:26 -0500

    Adding validate input and fixing some styles

--- js/index.js
+++ js/index.js
@@ -1,33 +1,71 @@
 const $form = document.querySelector("#form");
 const $btnSubmit = document.querySelector("#btn-submit");
 
-const URL = "https://bibliotecaunfv.herokuapp.com";
+const BASE_URL = "http://localhost:5500";
+const API_URL = "https://bibliotecaunfv.herokuapp.com";
 
+// Events
 $form.addEventListener("submit", async (ev) => {
   ev.preventDefault();
-  const userCode = document.querySelector("#user-code");
-  const data = await getUserById(userCode.value);
-  setLocalStorage(data);
-  window.location.href =
-    "https://mauroquinteros.github.io/reservar-libro/libros.html";
+  goToMainPage();
+});
+$btnSubmit.addEventListener("click", () => {
+  goToMainPage();
 });
 
-const getUserById = async (id) => {
-  try {
-    const response = await fetch(`${URL}/usuarios/${id}`);
-    const user = await response.json();
-    if (user.status) {
-      const { data } = user;
-      return data;
+// Main function
+async function goToMainPage() {
+  const userCode = document.querySelector("#user-code");
+  const validate = validateInput(userCode, "#input-alert");
+  if (validate) {
+    const { status, data, message } = await getUserById(userCode.value);
+    if (status) {
+      setLocalStorage(data);
+      window.location.href = `${BASE_URL}/books.html`;
     } else {
-      const { message } = user;
-      return message;
+      console.log(message);
+      validateInput(userCode, "#input-alert", message);
     }
+  }
+}
+
+function validateInput(inputCode, messageSelector, fetchMessage = "") {
+  if (inputCode.value === "") {
+    stylesAlert(inputCode, messageSelector, "Completa los campos.");
+    return false;
+  } else if (fetchMessage) {
+    stylesAlert(inputCode, messageSelector, fetchMessage);
+    return false;
+  } else {
+    stylesAlert(inputCode, messageSelector);
+    return true;
+  }
+}
+
+function stylesAlert(inputNode, inputSelector, message = "") {
+  const messageNode = document.querySelector(inputSelector);
+  if (message) {
+    messageNode.innerText = message;
+    inputNode.classList.add("border-danger");
+    inputNode.classList.remove("border-success");
+    messageNode.classList.add("text-danger");
+  } else {
+    inputNode.classList.remove("border-danger");
+    messageNode.innerHTML = "";
+  }
+}
+
+async function getUserById(id) {
+  try {
+    const response = await fetch(`${API_URL}/usuarios/${id}`);
+    const data = await response.json();
+    return data;
   } catch (error) {
     throw new Error(error.message);
   }
-};
+}
 
-const setLocalStorage = (data) => {
-  localStorage.setItem("userCode", JSON.stringify(data));
-};
+async function setLocalStorage(data) {
+  console.log(data);
+  localStorage.setItem("user", JSON.stringify(data));
+}
